9279db2bde780de80b9b452c9311d103a11fccb6
Gromeu2000 <45124641+Gromeu2000@users.noreply.github.com>
2018-11-28 12:14:05 +0100

    To finish

---
+++ Substring/Substring/Source.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int containsSubstring(const char * bigstring, const char * substring) {
+
+
+	for (int i = 0; i < ; i++) {
+
+
+		return 1;
+	}
+	
+}
+
+int main() {
+
+	system("pause");
+	return 0;
+
+}
